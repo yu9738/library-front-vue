eff22a7243b967d2ba75792c82f74b98f85a18e6
YSJ <yu9738@naver.com>
2024-03-05 00:15:15 +0900

    nav search delete

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -39,7 +39,7 @@ export default {
         <span class="fs-4">한공대 도서관</span>
       </a>
       <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
-        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+<!--        <input type="search" class="form-control" placeholder="Search..." aria-label="Search">-->
       </form>
     </div>
   </header>
